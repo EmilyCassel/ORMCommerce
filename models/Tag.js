f76cb885e00fab788d8fcc8e70f62302a38a5f79
emily <emilycassel77@gmail.com>
2024-03-03 15:45:00 -0500

    routes

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+   id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }, 
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -15,6 +23,7 @@ Tag.init(
     underscored: true,
     modelName: 'tag',
   }
+ 
 );
 
 module.exports = Tag;
